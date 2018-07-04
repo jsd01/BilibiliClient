a7b1461e9374ef4574c0a3d3ee23b33898e0c4d2
贾少东 <jacyffamily@163.com>
2018-07-04 15:26:54 +0800

    1.完成了直播页面的UI与数据请求 2.完成了图片缓存 3.下一步配置全局设置，调整MVP结构

---
+++ app/src/main/java/com/jsd/blibiliclient/app/base/SupportFragmentMe.java
@@ -0,0 +1,13 @@
+package com.jsd.blibiliclient.app.base;
+
+/**
+ * 项目名称：BlibiliClient
+ * 类描述：
+ * 创建人：贾少东
+ * 创建时间：2018-07-02 14:15
+ * 修改人：jsd
+ * 修改时间：2018-07-02 14:15
+ * 修改备注：
+ */
+public class SupportFragmentMe {
+}
---
+++ app/src/main/java/com/jsd/blibiliclient/app/config/applyOptions/intercept/UserAgentInterceptor.java
@@ -0,0 +1,22 @@
+package com.jsd.blibiliclient.app.config.applyOptions.intercept;
+
+import java.io.IOException;
+
+import okhttp3.Interceptor;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * 添加UA拦截器，B站请求API需要加上UA才能正常使用
+ */
+public class UserAgentInterceptor implements Interceptor {
+    @Override
+    public Response intercept(Chain chain) throws IOException {
+        Request originalRequest = chain.request();
+        /*Request requestWithUserAgent = originalRequest.newBuilder()
+                .removeHeader("User-Agent")
+                .addHeader("User-Agent", Api.COMMON_UA_STR)
+                .build();*/
+        return chain.proceed(originalRequest);
+    }
+}
